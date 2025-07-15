1dad46c51bfb657e9527597126c79284b5c21aa3
Prajith Jose <prajithjose007@gmail.com>
2025-07-15 12:17:32 +0000

    PS E:\wamp64\www\dutchpurchase\dutchpurchase1> npm run migrate:data
    > n

--- next.config.ts
+++ next.config.ts
@@ -9,9 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // This is required for the development environment.
-  // In a future version of Next.js, this will be the default.
-  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
 };
 
 export default nextConfig;
