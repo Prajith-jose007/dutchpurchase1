4811689d1895f89972684d472d1292d8ae74afe3
Praith Jose <praithjose007@gmail.com>
2025-07-21 14:59:45 +0400

    Local changes to package.json

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const https = require('https');
+const fs = require('fs');
+const next = require('next');
+const path = require('path');
+const express = require('express');
+
+const port = 3000;
+const dev = true;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, 'dutchoriental.ddns.net-key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'dutchoriental.ddns.net.pem')),
+};
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  https.createServer(httpsOptions, server).listen(port, () => {
+    console.log(`> Server listening on https://localhost:${port}`);
+  });
+});
