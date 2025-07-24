feafc79f9cd721b015a745a2d57e38f3ab3337b1
Prajith Jose <prajithjose007@gmail.com>
2025-07-24 07:33:26 +0000

    Module not found: Can't resolve './ZodError.js'
    ./node_modules/zod/v3/e

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,10 @@ const nextConfig: NextConfig = {
   },
   devIndicators: {
     allowedDevOrigins: ['dutchoriental.ddns.net'],
-  }
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['zod'],
+  },
 };
 
 export default nextConfig;
